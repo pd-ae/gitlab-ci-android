7d335530f3c85b2e3d04b1942aee236201d495c5
Matthias Fölser <matthias.foelser@plandata.at>
2019-09-26 11:14:51 +0200

    disable gradle daemon, fixed keystore path

--- start.sh
+++ start.sh
@@ -1,3 +1,5 @@
 #!/bin/sh
 
-[ -n "$KEYSTORE" ] && echo $KEYSTORE | base64 --decode > plandata.keystore
+[ -n "$KEYSTORE" ] && echo $KEYSTORE | base64 --decode > $CI_PROJECT_DIR/plandata.keystore
+
+export GRADLE_OPTS="-Dorg.gradle.daemon=false"
