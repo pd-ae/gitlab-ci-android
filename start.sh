bfbe391d4531fab9d52a49a6a0ca5aca493502cd
Matthias Fölser <matthias.foelser@plandata.at>
2019-09-26 13:25:47 +0200

    env var test

--- start.sh
+++ start.sh
@@ -1,5 +1,3 @@
 #!/usr/bin/env sh
 
-export > /export.log
-
-[ -n "$KEYSTORE" ] && echo $KEYSTORE | base64 --decode > $CI_PROJECT_DIR/plandata.keystore
+[ -n "$KEYSTORE" ] && mkdir -p $CI_PROJECT_DIR && echo $KEYSTORE | base64 --decode > $CI_PROJECT_DIR/plandata.keystore
